cceee10bde6dab910c7e1796edda35f829cdf6dc
Charles Karlo F. Leguarda <110873671+chrlskrlo@users.noreply.github.com>
2023-02-22 09:44:18 +0800

    First commit
    The data I used is from Poke API and I used axios to get the data. I also used .map to render each data and display it. .filter and .toLowerCase for the search bar to search a pokemon within each data in .map I also used switch for dark mode from material UI

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import '../input.css'
+
+const Card = () => {
+    return (
+        <>
+            <div className="card">
+                <h2>1</h2>
+                <img src="./images/chari.png" alt="" />
+                <h2>charmander</h2>
+            </div>
+
+        </>
+    )
+}
+export default Card;
